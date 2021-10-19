c41249b58e57a56fd1af7a626a98904503b04270
Viktor Speranskyy <vencedor@ukr.net>
2021-10-19 17:14:51 +0300

    2021.10

---
+++ src/com/itschool/classwork/ClassWork.java
@@ -0,0 +1,27 @@
+package com.itschool.classwork;
+
+public class ClassWork {
+   public static void main(String[] args) {
+      System.out.println("ClassWork:" + System.lineSeparator());
+   }
+
+   public static void task1() {
+
+   }
+
+   public static void task2() {
+
+   }
+
+   public static void task3() {
+
+   }
+
+   public static void task4() {
+
+   }
+
+   public static void task5() {
+
+   }
+}
